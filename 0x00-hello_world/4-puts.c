82101890f48d981649f536e65a8bdf157f191f98
lokwara <okwaralewis16@gmail.com>
2023-07-13 06:45:06 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -  This is main
+ * return - This is return
+ **/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
