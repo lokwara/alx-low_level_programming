676151b291a9886e9b6cf23286c5caf07d8663b1
lokwara <okwaralewis16@gmail.com>
2023-07-17 01:03:08 -0700

    7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main -Determines if a number is positive or negative
+ *
+ * Return:Always 0(Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a' ; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
