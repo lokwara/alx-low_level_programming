2391fd3404f0dc041f01789b0c5447ec817b4a09
lokwara <okwaralewis16@gmail.com>
2023-07-16 23:47:43 -0700

    2-print_alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,7 +6,12 @@
  */
 int main(void)
 {
-	putchar (65);
-	putchar ('\n');
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
 	return (0);
 }
