b9c848a769895644b6f9009b9974ff1cfd49ed5c
lokwara <okwaralewis16@gmail.com>
2023-07-17 00:29:07 -0700

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main -Determines if a number is positive or negative
+ *
+ * Return:Always 0(Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
