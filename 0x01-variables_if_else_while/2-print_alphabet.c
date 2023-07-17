24d48c109970c44ca496f8b6db29ff5511cd30af
lokwara <okwaralewis16@gmail.com>
2023-07-16 23:28:19 -0700

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main -Determines if a number is positive or negative
+ *
+ * Return:Always 0(Success)
+ */
+int main(void)
+{
+	putchar (97);
+	putchar ('\n');
+	return (0);
+}
