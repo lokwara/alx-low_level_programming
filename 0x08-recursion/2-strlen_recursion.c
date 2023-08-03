d3ff4ecbc3a25c28a3c516838b854c156979d0ab
lokwara <okwaralewis16@gmail.com>
2023-08-03 07:42:51 -0700

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen_recursion - print string in reverse
+ * @s : character
+ *
+ * Return : Nothing valid
+ */
+int _strlen_recursion(char *s)
+{
+	if(*s == '\0')
+	{
+		return (0);
+	}
+	s++;
+	return (_strlen_recursion(s) +1);
+}
